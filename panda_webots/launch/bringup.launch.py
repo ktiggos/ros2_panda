ff1c2a3861d580fd9b5e3f3dc1106ac800f8a64e
ktiggos <k.tiggos@gmail.com>
2026-01-11 16:32:25 +0200

    bringup: Launch rviz

--- panda_webots/launch/bringup.launch.py
+++ panda_webots/launch/bringup.launch.py
@@ -3,22 +3,24 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from ament_index_python.packages import get_package_share_directory
 
-PKG_NAME = 'panda_webots'
+WB = 'panda_webots'
+RVIZ = 'panda_viz'
 
 def generate_launch_description():
 
     ### Parameters ###
     # All parameters need to be type STR
     gui = "true"
-    world = "empty_world.wbt"
+    world = "milling_world.wbt"
     ##################
 
-    pkg_dir = get_package_share_directory(PKG_NAME)
+    webots_pkg = get_package_share_directory(WB)
+    rviz_pkg = get_package_share_directory(RVIZ)
 
-    launch_dir = os.path.join(pkg_dir,"launch/")
+    launch_dir = os.path.join(webots_pkg,"launch/")
 
     webots = IncludeLaunchDescription(
-        os.path.join(launch_dir,"webots.launch.py"),
+        os.path.join(os.path.join(webots_pkg,"launch/"),"webots.launch.py"),
         launch_arguments={'gui': gui, 'world': world}.items()
     )
 
@@ -26,7 +28,12 @@ def generate_launch_description():
         os.path.join(launch_dir, "robot_nodes.launch.py")
     )
 
+    rviz = IncludeLaunchDescription(
+        os.path.join(os.path.join(rviz_pkg,"launch/"),"panda_viz.launch.py")
+    )
+
     return LaunchDescription([
         webots,
-        robot_nodes
+        robot_nodes,
+        rviz,
     ])
